235fe92b84a12fdcd1eb54a8a2243b38cbf6bf89
mac <mac@MacBook-Pro.local>
2025-12-17 22:39:46 +0100

    deploy

--- Pacific-Host-Hospitality-Dashboard/vite.config.ts
+++ Pacific-Host-Hospitality-Dashboard/vite.config.ts
@@ -21,14 +21,14 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.url, "../client/src"),
-      "@shared": path.resolve(import.meta.url, "../shared"),
-      "@assets": path.resolve(import.meta.url, "../attached_assets"),
+      "@": path.resolve(__dirname, "client/src"),
+      "@shared": path.resolve(__dirname, "shared"),
+      "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(import.meta.url, "client"),
+  root: path.resolve(__dirname, "client"), // <- folder where index.html lives
   build: {
-    outDir: path.resolve(import.meta.url, "dist"), // <-- change to dist
+    outDir: path.resolve(__dirname, "docs"), // <- output folder for GitHub Pages
     emptyOutDir: true,
   },
   server: {
